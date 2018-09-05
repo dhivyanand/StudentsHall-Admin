b1330fec85cb707a7fd1940ef1e7979731d142fb
dhivyanand <dhivyananthan98@gmail.com>
2018-09-05 22:00:05 +0530

    Added with date picker function to Exam Activity

--- app/src/main/java/com/example/system/stuentzhall_admin/Exam.java
+++ app/src/main/java/com/example/system/stuentzhall_admin/Exam.java
@@ -3,23 +3,57 @@ package com.example.system.stuentzhall_admin;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Exam extends AppCompatActivity {
+
+    EditText date;
+    Button add;
+    Calendar myCalendar;
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+        date.setText(sdf.format(myCalendar.getTime()));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
 
-        DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        date = (EditText)findViewById(R.id.date);
+        myCalendar = Calendar.getInstance();
+
+
+        final DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
-                // arg1 = year
-                // arg2 = month
-                // arg3 = day
+
+                myCalendar.set(Calendar.YEAR, arg1);
+                myCalendar.set(Calendar.MONTH, arg2);
+                myCalendar.set(Calendar.DAY_OF_MONTH, arg3);
+                updateLabel();
+
             }
         };
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(Exam.this, myDateListener, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
 
