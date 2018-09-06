0a4a47e5bee8a00016c12c067693e871ce4c5310
dhivyanand <dhivyananthan98@gmail.com>
2018-09-06 22:21:17 +0530

    Created Hall Activity

---
+++ app/src/main/java/com/example/system/stuentzhall_admin/hall.java
@@ -0,0 +1,13 @@
+package com.example.system.stuentzhall_admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hall);
+    }
+}
